f84a819c70e6d02c2b160048520e794d0716b560
Nick Corfmat <ncorfmat@ucsc.edu>
2024-10-12 16:38:17 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ const APP_NAME = "Fun Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const sketchpadTitle = document.createElement("h1");
+sketchpadTitle.textContent = APP_NAME;
+
+app.append(sketchpadTitle);
+
+const sketchCanvas = document.createElement("canvas");
+sketchCanvas.width = 256;
+sketchCanvas.height = 256;
+
+app.append(sketchCanvas);
