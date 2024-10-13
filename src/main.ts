910352f6569359019b8e279096dee15d41129bcf
Nick Corfmat <ncorfmat@ucsc.edu>
2024-10-12 17:07:53 -0700

    added drawing feature

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,69 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 
+// Global variables
+let isDrawing = false;
+let strokeX = 0;
+let strokeY = 0;
+
 const sketchpadTitle = document.createElement("h1");
 sketchpadTitle.textContent = APP_NAME;
 
 app.append(sketchpadTitle);
 
+// Canvas setup
 const sketchCanvas = document.createElement("canvas");
 sketchCanvas.width = 256;
 sketchCanvas.height = 256;
 
+const context = sketchCanvas.getContext("2d");
+
+if (context) {
+  context.strokeStyle = "black";
+  context.lineWidth = 2;
+} else {
+    throw new Error('Context not found.');
+}
+
 app.append(sketchCanvas);
+
+// Enable drawing on canvas
+// Source: MDN web docs, https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+sketchCanvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  strokeX = e.offsetX;
+  strokeY = e.offsetY;
+});
+
+sketchCanvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(context!, strokeX, strokeY, e.offsetX, e.offsetY);
+    strokeX = e.offsetX;
+    strokeY = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(context!, strokeX, strokeY, e.offsetX, e.offsetY);
+    strokeX = 0;
+    strokeY = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(
+  context: CanvasRenderingContext2D,
+  startX: number,
+  startY: number,
+  endX: number,
+  endY: number
+) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(startX, startY);
+  context.lineTo(endX, endY);
+  context.stroke();
+  context.closePath();
+}
